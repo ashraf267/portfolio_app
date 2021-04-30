4f0db70f7836beff18220674a0a1100a2d7f6bc0
Ashraf Otagun <Otagunolaniyi7@gmail.com>
2021-04-30 11:11:44 +0100

    Yes! Done with the app

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white70,
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -17,76 +18,126 @@ class MyApp extends StatelessWidget {
             Image(
               image: AssetImage('images/dev.jpg'),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Ashraf',
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    fontFamily: 'Pacifico',
-                    fontWeight: FontWeight.bold,
+            Divider(
+              color: Colors.blueAccent,
+              height: 3.0,
+              thickness: 3.0,
+            ),
+            Container(
+              margin: EdgeInsets.all(7.0),
+              padding: EdgeInsets.all(3.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'n3y0,',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Text(
-                  'Flutter developer',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontFamily: 'Mukta',
-                    letterSpacing: 1.0,
+                  Text(
+                    '- Flutter developer',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Mukta',
+                      letterSpacing: 1.0,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             SizedBox(
-              height: 20.0,
+              height: 15.0,
             ),
-            Row(
-              children: [
-                Text(
-                  'Job:',
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontFamily: 'Mukta',
-                    letterSpacing: 1.0,
+            Container(
+              margin: EdgeInsets.all(6.0),
+              padding: EdgeInsets.all(2.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Job:',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontFamily: 'Mukta',
+                      letterSpacing: 1.0,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'Intern at Zuri Training',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontFamily: 'Mukta',
+                  SizedBox(
+                    width: 10.0,
                   ),
-                ),
-              ],
+                  Text(
+                    'Intern at Zuri Training',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Mukta',
+                    ),
+                  ),
+                ],
+              ),
             ),
             SizedBox(
-              height: 10.0,
+              height: 5.0,
             ),
-            Row(
-              children: [
-                Text(
-                  'Level:',
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontFamily: 'Mukta',
-                    letterSpacing: 1.0,
+            Container(
+              margin: EdgeInsets.all(6.0),
+              padding: EdgeInsets.all(2.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Level:',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontFamily: 'Mukta',
+                      letterSpacing: 1.0,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'Jnr(1 - 2 yrs)',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontFamily: 'Mukta',
+                  SizedBox(
+                    width: 10.0,
                   ),
+                  Text(
+                    'Jnr(1 - 2 yrs)',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Mukta',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    'About me',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Mukta',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 3.0,
+                    width: 85.0,
+                    child: Divider(
+                      color: Colors.blueAccent,
+                      thickness: 2.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(7.0),
+              padding: EdgeInsets.all(3.0),
+              child: Text(
+                'Hi guys, my name is Ashraf Otagun Olaniyi (n3y0). I am a Software developer building cross-platform mobile apps with Flutter. Futhermore, I am an undergraduate student at the University of Ilorin studying Computer science.',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontFamily: 'Mukta',
                 ),
-              ],
+              ),
             ),
           ],
         ),
